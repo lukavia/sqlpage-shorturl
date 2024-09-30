a89b6477804f5f18243972258e1ce334ab985fe9
Anton Avramov <lukav@lukav.com>
2024-09-30 12:57:49 -0400

    Fix redirects

--- manage/create.sql
+++ manage/create.sql
@@ -1,5 +1,5 @@
-SELECT 'redirect' AS component, '/login.sql' AS link
+SELECT 'redirect' AS component, 'login.sql' AS link
     WHERE NOT EXISTS(SELECT * FROM user_sessions WHERE session_id = sqlpage.cookie('session_id'));
 
 INSERT INTO urls(path, url) VALUES(:path,:url)
-  RETURNING 'redirect' AS component, '/' AS link;
+  RETURNING 'redirect' AS component, 'index.sql' AS link;
--- manage/delete.sql
+++ manage/delete.sql
@@ -1,5 +1,5 @@
-SELECT 'redirect' AS component, '/login.sql' AS link
+SELECT 'redirect' AS component, 'login.sql' AS link
     WHERE NOT EXISTS(SELECT * FROM user_sessions WHERE session_id = sqlpage.cookie('session_id'));
 
 DELETE FROM urls WHERE path = $path
-  RETURNING 'redirect' AS component, '/' AS link;
+  RETURNING 'redirect' AS component, 'index.sql' AS link;
